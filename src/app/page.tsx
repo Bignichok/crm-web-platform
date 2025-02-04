01e30b060811ea7a35259617e6ceb83975ddd57a
bignichok <inikita546@gmail.com>
2025-02-04 16:03:15 +0200

    add company form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import StatusLabel, { Status } from "@/app/components/statusLabel";
+import AddCompanyButton from "@/app/components/addCompanyButton";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.Inactive}>Inactive</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }
